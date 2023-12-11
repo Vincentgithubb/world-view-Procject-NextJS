22666f377bafd6e5fbebdec7e687a673c7ae04a3
Vincentgithubb <vincinant@gmail.com>
2023-12-11 21:33:06 +0100

    push amélioration css

--- my-app/src/components/CountryCard.tsx
+++ my-app/src/components/CountryCard.tsx
@@ -1,26 +1,44 @@
+'use client'
 import Link from 'next/link'
 import Input from 'postcss'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 type countrycardProps = {
   country: any
 }
 
 const CountryCard = ({ country }: countrycardProps) => {
+  const [isHovered, setIsHovered] = useState(false)
+
+  const handleMouseEnter = () => {
+    setIsHovered(true)
+  }
+
+  const handleMouseLeave = () => {
+    setIsHovered(false)
+  }
+
   return (
-    <Link
-      href={{
-        pathname: '/pagedetails',
-        query: {
-          ccn3: `${country.ccn3}`
-        }
-      }}
+    <div
+      className={`card ${isHovered ? 'hovered' : ''}`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
-      <div className="card">
-        <h1 className="name">{country.name.common}</h1>
-        <img className="flag" src={country.flags.svg} />
-      </div>
-    </Link>
+      <Link
+        href={{
+          pathname: '/pagedetails',
+          query: {
+            ccn3: `${country.ccn3}`
+          }
+        }}
+      >
+        <div className="card">
+          <h1 className="name">{country.name.common}</h1>
+          <img className="flag" src={country.flags.svg} />
+          {isHovered && <p>Details</p>}
+        </div>
+      </Link>
+    </div>
   )
 }
 export default CountryCard
--- my-app/src/components/Navbar.tsx
+++ my-app/src/components/Navbar.tsx
@@ -1,29 +1,32 @@
-import { useEffect } from 'react'
-import Input from 'postcss/lib/input'
-import Link from 'next/link'
+// Navbar.tsx
 
-type navProps = {
+import React from 'react'
+
+type NavProps = {
   handleSearch: any
   handleContinentChange: (event: React.ChangeEvent<HTMLSelectElement>) => void
 }
 
-const Navbar = ({ handleSearch, handleContinentChange }: navProps) => {
+const Navbar = ({ handleSearch, handleContinentChange }: NavProps) => {
   return (
-    <div className="topnav">
-      <a className="active" href="/">
-        World View
-      </a>
-      <input className="search" type="text" placeholder="Search" onChange={handleSearch} />
-      <select onChange={handleContinentChange}>
-        <option value="">tout les Continents</option>
-        <option value="Africa">Afrique</option>
-        <option value="Asia">Asie</option>
-        <option value="Europe">Europe</option>
-        <option value="North America">Amérique du nord</option>
-        <option value="South America">Amérique du sud</option>
-        <option value="Europe">Europe</option>
-      </select>
-    </div>
+    <>
+      <div className="topnav">
+        <div className="frise"></div>
+        <a className="active" href="/">
+          World View
+        </a>
+        <input className="search" type="text" placeholder="Search" onChange={handleSearch} />
+        <select onChange={handleContinentChange}>
+          <option value="">Tous les Continents</option>
+          <option value="Africa">Afrique</option>
+          <option value="Asia">Asie</option>
+          <option value="Europe">Europe</option>
+          <option value="North America">Amérique du nord</option>
+          <option value="South America">Amérique du sud</option>
+          <option value="Europe">Europe</option>
+        </select>
+      </div>
+    </>
   )
 }
 
